4b7da2830c471f5ae21ad75be383fd0ce90f6e29
hi-dil <mohamadhaidilidris@hotmail.com>
2022-01-01 00:10:28 +0800

    second commit

--- src/app.js
+++ src/app.js
@@ -1,38 +1,53 @@
 import tmi from 'tmi.js'
-import { Client } from 'tmi.js';
-import { username } from 'tmi.js/lib/utils';
-import { BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME } from './constant';
+import {
+    Client
+} from 'tmi.js';
+import {
+    username
+} from 'tmi.js/lib/utils';
+import {
+    BOT_USERNAME,
+    OAUTH_TOKEN,
+    CHANNEL_NAME
+} from './constant';
 
 const options = {
-    options: {debug: true },
+    options: {
+        debug: true
+    },
     connection: {
-        reconnect: true, 
+        reconnect: true,
         secure: true
     },
     identity: {
-        username: BOT_USERNAME, 
+        username: BOT_USERNAME,
         password: OAUTH_TOKEN
     },
     channels: CHANNEL_NAME
 }
 
 const twoArgsRegex = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?([a-zA-Z0-9]+)?(?:\W+)?(.*)?/);
-const oneArgsRegex = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
-const noArgsRegex = new RegExp(/^!([a-zA-Z0-9]+)?/);
+const tenshiRegex = new RegExp(/tenshiWut/gi);
 
 let active = true;
 let timeout;
 
 String.prototype.toHHMMSS = function () {
     var sec_num = parseInt(this, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
+    var hours = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var time = hours + ':' + minutes + ':' + seconds;
     return time;
 }
 
@@ -41,74 +56,97 @@ const client = new tmi.Client(options);
 
 client.connect();
 client.on('message', (channel, userstate, message, self) => {
-    
+
     if (self) return;
 
-    if (message.match(twoArgsRegex)){
+    // if (message.match(tenshiRegex)){
+    //     client.say(channel, `TenshiWut 🤙 WUUUUT`);
+    // }
+
+    if (!message.startsWith('!')) return;
+
+    if (message.match(twoArgsRegex)) {
         const [raw, command, argument1, argument2] = message.match(twoArgsRegex);
 
-        if (command === 'pyramid') {
+        if (command === 'pyramid' || command ==='p') {
 
             if (active) {
                 let limit = 0;
-                if (channel === '#bassnix'){
-                    limit = 40;
+                if (channel === '#bassnix' || channel === '#bassnixBOT') {
+                    limit = 50;
                 } else {
                     limit = 10;
                 }
-    
-                if (!argument2) return;
-    
+
                 const size = parseInt(argument2)
                 const emote = argument1 + ' ';
-        
-                if (size<=limit){
-                    for (let i = 1; i <= size; i++) {
-                        client.say(channel, `${emote.repeat(i)}`);
+                const emoteMaxSize = emote.length * size;
+
+                if (emoteMaxSize <= 500) {
+                    if (size <= limit) {
+                        for (let i = 1; i <= size; i++) {
+                            client.say(channel, `${emote.repeat(i)}`);
+                        }
+    
+                        for (let i = (size - 1); i > 0; i--) {
+                            client.say(channel, `${emote.repeat(i)}`);
+                        }
+                    } else {
+                        client.say(channel, `Please put less or equal than ${limit} emote`)
                     }
-            
-                    for (let i = (size - 1); i > 0; i--) {
-                        client.say(channel, `${emote.repeat(i)}`);
-                    }
+    
+                    active = false
+                    timeout = setTimeout(() => {
+                        active = true
+                    }, 30000)
                 } else {
-                    client.say(channel, `Please put less or equal than ${limit} emote`)
+                    client.say(channel, `LULE exceed max characters limit (${emoteMaxSize}/500)`)
                 }
 
-                active = false
-                timeout = setTimeout(() => {
-                    active = true
-                }, 30000)
+                
             } else {
                 client.say(channel, `DOCING wait for ${getTimeLeft(timeout)}s`);
             }
-
-            
         }
 
         if (command === 'tank') {
             client.say(channel, `█ ${argument1} █ █ ] ▄▄▄▄▄▄▄ ${argument2} ............ ▂▄▅█████████▅▄▃▂ ........... ███████████████████ ] ........... ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤`)
         }
-    } else if (message.match(oneArgsRegex)) {
-        const [raw, command, argument] = message.match(oneArgsRegex);
-    } 
+    }
 
-    if (message === '!ping') {
+    const args = message.slice(1).split(' ');
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === 'fill') {
+        let word = args.join(' ');
+        let concatWord = '';
+        let limit = 500;
+        let length = 0;
+
+        while (length < limit) {
+            concatWord = concatWord.concat(" ", word);
+            length = concatWord.length + 1;
+        }
+
+        client.say(channel, `${concatWord}`);
+
+    }
+
+    if (cmd === 'ping') {
         let time = process.uptime();
         let uptime = (time + "").toHHMMSS();
         client.say(channel, `MrDestructoid bassnixBOT has been running for ${uptime} `);
     }
+
+    if (cmd === 'eg') {
+        client.say(channel, `=eg`);
+    }
+
+    if (cmd === 'gibeg') {
+        client.say(channel, `=gibeg bassnix all`)
+    }
 });
 
-async function getdata(url) {
-    const response = fetch(url);
-    const data = await response.json();
-    const { quote } = data;
-
-    return quote;
-}
-
 function getTimeLeft(timeout) {
-    return Math.ceil((timeout._idleStart + timeout._idleTimeout)/1000 - process.uptime());
+    return Math.ceil((timeout._idleStart + timeout._idleTimeout) / 1000 - process.uptime());
 }
-
-
